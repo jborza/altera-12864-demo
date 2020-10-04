b47198fc91d2bf54b1e519d28a6209ad675188c9
jborza <jborza@gmail.com>
2020-10-04 21:52:26 +0200

    better ram generator

--- generate_ramtxt.py
+++ generate_ramtxt.py
@@ -1,9 +1,19 @@
+import sys
 
 def str2bin(text):
     for i in range(0, len(text)):
         char = ord(text[i])
         print(f'{char:08b}')
 
-txt3 = "dolor sit amet"
+txt = []
+txt.append("Hello World!    ")
+txt.append("12864 LCD Screen")
+txt.append("Lorem ipsum     ")
+txt.append("dolor sit amet. ")
 
-str2bin(txt3)
+str2bin(txt[0])
+
+for i in range(0,4):
+    with open(f'ram{i}.txt', 'w') as f:
+        sys.stdout = f
+        str2bin(txt[i])

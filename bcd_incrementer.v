74da60490b6e774eb0094399f83c52ef1067885b
jborza <jborza@gmail.com>
2020-10-22 19:14:42 +0200

    attempt at bcd incrementer

--- bcd_incrementer.v
+++ bcd_incrementer.v
@@ -12,21 +12,22 @@ module bcd_incrementer(
 //inputs, outputs
 input wire [11:0] bcd_in;
 output wire [11:0] bcd_out;
+localparam nine = 4'b1001;
+localparam ninenine = 8'b10011001;
+localparam one  = 4'b0001;
+localparam zero = 4'b0000;
 
-//we carry 
+//carry on nine
 //digit one = add + carry out
 wire [4:0] digit2;
-assign digit2 = bcd_in[3:0] + 1;
+assign digit2 = bcd_in[3:0] == nine ? zero : bcd_in[3:0] + 1;
 
-//wire carry2 = digit2[4];
+wire [4:0] digit1; //carry over first nine
+assign digit1 = bcd_in[3:0] == nine ? bcd_in[7:4] + one: bcd_in[7:4];
 
-wire [4:0] digit1;
-assign digit1 = bcd_in[7:4] + digit2[4];
-
-wire [3:0] digit0;
-assign digit0 = bcd_in[11:8] + digit1[4];
+wire [3:0] digit0; //carry over second nine
+assign digit0 = bcd_in[7:0] == ninenine ? bcd_in[11:8] + one : bcd_in[11:8];
 
 assign bcd_out = {digit0[3:0], digit1[3:0], digit2[3:0]};
-//assign bcd_out = bcd_in;
 
 endmodule

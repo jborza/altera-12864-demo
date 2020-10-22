54950ed430ad6829865f3569fd64930ba579b872
jborza <jborza@gmail.com>
2020-10-22 19:25:06 +0200

    bcd_incrementer passing tests

--- bcd_incrementer.v
+++ bcd_incrementer.v
@@ -14,6 +14,7 @@ input wire [11:0] bcd_in;
 output wire [11:0] bcd_out;
 localparam nine = 4'b1001;
 localparam ninenine = 8'b10011001;
+localparam nineninenine = 12'b100110011001;
 localparam one  = 4'b0001;
 localparam zero = 4'b0000;
 
@@ -23,10 +24,11 @@ wire [4:0] digit2;
 assign digit2 = bcd_in[3:0] == nine ? zero : bcd_in[3:0] + 1;
 
 wire [4:0] digit1; //carry over first nine
-assign digit1 = bcd_in[3:0] == nine ? bcd_in[7:4] + one: bcd_in[7:4];
+assign digit1 = bcd_in[7:0] == ninenine ? zero 
+												: bcd_in[3:0] == nine ? bcd_in[7:4] + one : bcd_in[7:4];
 
 wire [3:0] digit0; //carry over second nine
-assign digit0 = bcd_in[7:0] == ninenine ? bcd_in[11:8] + one : bcd_in[11:8];
+assign digit0 = bcd_in[12:0] == nineninenine ? zero : bcd_in[7:0] == ninenine ? bcd_in[11:8] + one : bcd_in[11:8];
 
 assign bcd_out = {digit0[3:0], digit1[3:0], digit2[3:0]};
 
